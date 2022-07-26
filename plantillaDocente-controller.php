e37344733d682aec2d6a52af65ff76e753ef3bdd
IrvingNaranjo <102385585+IrvingNaranjo@users.noreply.github.com>
2022-07-26 16:36:54 -0500

    Actualización de componentes responsivos

--- plantillaDocente-controller.php
+++ plantillaDocente-controller.php
@@ -13,7 +13,6 @@
         $Codigo = "";
         $cont = 1;
         if($ResultSet->num_rows > 0){
-            $Codigo = "<div class='container'>";
             while($row = $ResultSet->fetch_assoc()){
                 $id_maestro = $row['id_maestro'];
                 $nombre_maestro = $row['nombre_maestro'];
@@ -22,43 +21,46 @@
                 $materias_maestro = $row['materias_maestro'];
                 $contacto_maestro = $row['contacto_maestro'];
                 $url_imagen = $row['url_imagen'];
-                if($cont==1){
-                    $Codigo .= "<div class='col'>
-                                <div class='row'>";
-                }
-                $Codigo .=  "<div class='col gris-zinc m-2'>
-                                <div class='d-flex flex-row justify-content-center my-2'>
-                                    <img class='rounded-circle p-1 bg-primary' src='img/hector_trujillo.JPG' alt='' style='width: 10rem; height: 10rem;'>
-                                </div>
-                                <div class='d-flex flex-row justify-content-center mx-2'>
-                                    <h5 class='fs-5 fw-bolt'>" .$nombre_maestro. "</h5>
-                                </div>
-                                <div class='d-flex flex-row justify-content-center my-2'>
-                                    <button type='button' class='btn amarillo fw-bolt' data-bs-toggle='modal' data-bs-target='#ModalDocente".$id_maestro."'>Ver mas</button>
+                $Codigo .=  "<div class='col-lg-4 col-md-6 col-sm-9 col-8 my-2'>
+                                <div class='d-flex flex-column gris-zinc rounded-3'>
+                                    <div class='d-flex flex-row justify-content-center my-2'>
+                                        <img class='rounded-circle p-1 bg-primary' src='img/hector_trujillo.JPG' alt='' style='width: 10rem; height: 10rem;'>
+                                    </div>
+                                    <div class='d-flex flex-row justify-content-center mx-2'>
+                                        <h5 class='fs-5 fw-bolt'>" .$nombre_maestro. "</h5>
+                                    </div>
+                                    <div class='d-flex flex-row justify-content-center my-2'>
+                                        <button type='button' class='btn amarillo fw-bolt' data-bs-toggle='modal' data-bs-target='#ModalDocente".$id_maestro."'>Ver mas</button>
+                                    </div>
                                 </div>
                             </div>";
                 
                 $Codigo .=   "<div class='modal fade' id='ModalDocente".$id_maestro."' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
                                 <div class='modal-dialog modal-lg'>
-                                    <div class='modal-content gris-zinc'>
-                                        
-                                        <svg xmlns='http://www.w3.org/2000/svg' aria-hidden='true' role='img' class='cursor-pointer p-1' style='width: 3rem; height: 3rem;' 
-                                            preserveAspectRatio='xMidYMid meet' viewBox='0 0 512 512' data-bs-dismiss='modal' aria-label='Close'>
-                                            <path fill='currentColor' d='m289.94 256l95-95A24 24 0 0 0 351 127l-95 95l-95-95a24 24 0 0 0-34 
-                                            34l95 95l-95 95a24 24 0 1 0 34 34l95-95l95 95a24 24 0 0 0 34-34Z'/>
-                                        </svg>
-                                        
+                                    <div class='modal-content'>
+                                        <div class='modal-header azul-medio'>
+                                            <h5 class='modal-title text-white font-semibold'>Información docente</h5>
+                                            <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+                                        </div>
                                         <div class='modal-body'>
                                             <div class='col'>
                                             <div class='row justify-content-center m-2'>
-                                                <div class='col justify-content-center m-2'>
+                                                <div class='col-7 col-lg-auto justify-content-center m-2'>
                                                     <div class='d-flex justify-content-center'>
                                                         <img class='rounded-circle p-1 bg-primary' src='img/hector_trujillo.JPG' alt='' style='width: 15rem; height: 15rem;'>
                                                     </div>
                                                 </div>
-                                                <div class='col-12 col-lg-7 justify-content-center align-middle m-2'>
-                                                    <h1 class='amarillo text-center py-2 fs-4'>$nombre_maestro</h1>         
-                                                    <p class='text-dark text-center'>$palabras_maestro</p>
+                                                <div class='col-12 col-lg-7 justify-content-center align-items-center m-2'>
+                                                    <div class='d-flex align-items-center h-100'>
+                                                        <div class='col'>
+                                                            <div class='row'>
+                                                                <h1 class='amarillo text-center py-2 fs-4'>$nombre_maestro</h1> 
+                                                            </div>      
+                                                            <div class='row'>
+                                                                <p class='text-dark text-center'>$palabras_maestro</p>
+                                                            </div>     
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
                                             </div>
@@ -75,14 +77,7 @@
                                     </div>
                                 </div>
                             </div>";
-                $cont = $cont + 1;
-                if($cont==4){
-                    $Codigo .= "</div>";
-                    $Codigo .= "</div>";
-                    $cont = 1;
-                }
             }
-            $Codigo .= "</div>";
         }
         return $Codigo;
     }
