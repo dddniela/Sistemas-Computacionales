a2089c9fdc57e12251f7b1864db5d3889e555c9f
Angel Sanchez <69773924+AtkarMX@users.noreply.github.com>
2023-02-24 22:03:07 -0600

    Detalles en Inicio
    + Se ordenaron los estilos CSS.
    + Se arreglaron enlaces en Inicio.
    + Se corrigieron animaciones en Inicio.

--- src/View/inicio.php
+++ src/View/inicio.php
@@ -13,9 +13,9 @@
         <img src="../img/ITVER-1.webp" class="d-block w-100 align-middle" width="100%" height="100%">
         <div class="container">
           <div class="carousel-caption d-flex flex-column">
-            <h1 class="tituloPrincipal text-center font-bold m-2">Ingeniería en Sistemas Computacionales</h1>
-            <h2 class="subtituloPrincipal text-center font-bold">Desarrolla nuevos horizontes</h1>
-              <p><a class="botonPrincipal btn font-bold d-none d-md-inline-flex" href="?pag=3">Explorar</a>
+            <h1 class="mainTitle text-center font-bold m-2">Ingeniería en Sistemas Computacionales</h1>
+            <h2 class="mainSubtitle text-center font-bold">Desarrolla nuevos horizontes</h1>
+              <p><a class="mainButton btn font-bold d-none d-md-inline-flex" href="?option=3">Explorar</a>
               </p>
           </div>
         </div>
@@ -25,9 +25,9 @@
         <img src="../img/IMG_3929.webp" class="d-block w-100 align-middle" width="100%" height="100%">
         <div class="container">
           <div class="carousel-caption">
-            <h1 class="tituloPrincipal text-center font-bold m-2">Ingeniería en Sistemas Computacionales</h1>
-            <h2 class="subtituloPrincipal text-center font-bold">Aprende nuevas tecnologías</h1>
-              <p><a class="botonPrincipal btn font-bold d-none d-md-inline-flex" href="?option=4">Explorar</a></p>
+            <h1 class="mainTitle text-center font-bold m-2">Ingeniería en Sistemas Computacionales</h1>
+            <h2 class="mainSubtitle text-center font-bold">Aprende nuevas tecnologías</h1>
+              <p><a class="mainButton btn font-bold d-none d-md-inline-flex" href="?option=1">Explorar</a></p>
           </div>
         </div>
       </div>
@@ -36,9 +36,9 @@
         <img src="../img/IMG_1891.webp" class="d-block w-100 align-middle" width="100%" height="100%">
         <div class="container">
           <div class="carousel-caption">
-            <h1 class="tituloPrincipal text-center font-bold m-2">Ingeniería en Sistemas Computacionales</h1>
-            <h2 class="subtituloPrincipal text-center font-bold d-none d-md-inline-flex">Colabora con la comunidad</h1>
-              <!-- <p><a class="botonPrincipal btn font-bold" href="ofertaEducativa.php">Explorar</a></p> -->
+            <h1 class="mainTitle text-center font-bold m-2">Ingeniería en Sistemas Computacionales</h1>
+            <h2 class="mainSubtitle text-center font-bold d-none d-md-inline-flex">Colabora con la comunidad</h1>
+            <p><a class="mainButton btn font-bold d-none d-md-inline-flex" href="?option=5">Explorar</a></p>
           </div>
         </div>
       </div>
@@ -56,26 +56,26 @@
 </section>
 <!--Fin Carrusel de portada -->
 
-<!-- Información carrera -->
+<!-- Razones para estudiar la carrera -->
 <section class="p-2">
   <div class="row px-2 g-0">
     <div class="col-lg-6 col-12 p-2 shadow-sm">
       <div class="d-flex justify-content-center align-items-center w-100 h-100">
-        <img src="../img/Servicio social-3.webp" class="img-fluid rounded" alt="">
+        <img class="img-fluid rounded" src="../img/Servicio social-3.webp" alt="">
       </div>
     </div>
 
-    <div class="seccionSistemas col-lg-6 col-12 p-4">
+    <div class="lightSection col-lg-6 col-12 p-4">
       <div class="d-flex justify-content-center align-items-center w-100 h-100">
         <div class="row g-0">
           <div class="col-12">
-            <h2 class="tituloSeccion text-center font-bold m-3">¿Por qué estudiar Ingeniería en Sistemas
+            <h2 class="sectionTitle text-center font-bold m-3">¿Por qué estudiar Ingeniería en Sistemas
               Computacionales?</h2>
-            <div class="separadorSeccion"></div>
+            <div class="sectionSeparator"></div>
           </div>
           <div class="col-12" style="text-align: center;">
-            <p style="text-align: justify;"> Afronta el mundo actual y los conocimientos para forjar el futuro,
-              permitiendo el desarrollo y la creación de nuevas aplicaciones o sistemas que puedan ser de ayuda
+            <p style="text-align: justify;"> La carrera de ingeniería en sistemas computacionales permite al estudiante la oportunidad de 
+              afrontar el mundo actual y obtener los conocimientos necesarios para forjar el futuro, a través del desarrollo y la creación de nuevas aplicaciones o sistemas que puedan ser de ayuda
               para una empresa y la sociedad en general.
             </p>
             <a class="btn btn-warning font-bold" href="?option=3" role="button">¡Saber más!</a>
@@ -85,71 +85,72 @@
     </div>
   </div>
 </section>
-<!--Fin Información carrera -->
+<!--Fin Razones para estudiar la carrera -->
 
 <!-- Areas de aprendizaje -->
-<section class="seccionArea bg-light p-2">
-  <h2 class="tituloSeccion text-center font-bold m-3">Áreas de aprendizaje</h2>
-  <div class="separadorSeccion"></div>
+<section class="lightSection bg-light p-2">
+  <h2 class="sectionTitle text-center font-bold m-3">Áreas de aprendizaje</h2>
+  <div class="sectionSeparator"></div>
 
   <div class="row g-0">
-    <div class="col-lg-4 col-sm-6 text-center p-3">
-      <div class="area shadow-sm p-4">
-        <a href="?option=3.php">
-          <img class="imagenArea items-center" src="../img/topicos/programacion-icono.PNG" alt="">
-        </a>
-        <h3 class="tituloArea text-center font-bold text-xl">Desarrollo de Software</h3>
-        <p class="textoArea">Desarrolla programas a través de diferentes lenguajes de programación.</p>
-      </div>
-    </div>
-
-    <div class="col-lg-4 col-sm-6 text-center p-3">
-      <div class="area shadow-sm p-4">
-        <a href="?option=3">
-          <img class="imagenArea items-center" src="../img/topicos/ing-software-icono.PNG" alt="">
-        </a>
-        <h3 class="tituloArea text-center font-bold text-xl">Ingeniería de Software</h3>
-        <p class="textoArea">Aplica metodologías ágiles para el desarrollo eficiente de proyectos de software.</p>
-      </div>
-    </div>
 
     <div class="col-lg-4 col-sm-6 text-center p-3">
       <div class="area shadow-sm p-4">
         <a href="?option=3.php">
-          <img class="imagenArea items-center" src="../img/topicos/admon-bd-icono.PNG" alt="">
+          <img class="areaImage items-center" src="../img/topicos/programacion-icono.PNG" alt="">
         </a>
-        <h3 class="tituloArea text-center font-bold text-xl">Administración de Bases de Datos</h3>
-        <p class="textoArea">Gestiona y almacena el flujo de datos de información de un sistema informático.</p>
+        <h3 class="areaTitle text-center font-bold text-xl">Desarrollo de Software</h3>
+        <p class="areaText">Desarrolla programas a través de diferentes lenguajes de programación.</p>
       </div>
     </div>
 
     <div class="col-lg-4 col-sm-6 text-center p-3">
       <div class="area shadow-sm p-4">
         <a href="?option=3">
-          <img class="imagenArea items-center" src="../img/topicos/programacion-web-icono.PNG" alt="">
+          <img class="areaImage items-center" src="../img/topicos/ing-software-icono.PNG" alt="">
         </a>
-        <h3 class="tituloArea text-center font-bold text-xl">Programación web</h3>
-        <p class="textoArea">Crea y desarrolla aplicaciones y sitios web para brindar soluciones a las empresas.</p>
+        <h3 class="areaTitle text-center font-bold text-xl">Ingeniería de Software</h3>
+        <p class="areaText">Aplica metodologías ágiles para el desarrollo eficiente de proyectos de software.</p>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-6 text-center p-3">
+      <div class="area shadow-sm p-4">
+        <a href="?option=3.php">
+          <img class="areaImage items-center" src="../img/topicos/admon-bd-icono.PNG" alt="">
+        </a>
+        <h3 class="areaTitle text-center font-bold text-xl">Bases de Datos</h3>
+        <p class="areaText">Gestiona y almacena el flujo de datos de información de un sistema informático.</p>
       </div>
     </div>
 
     <div class="col-lg-4 col-sm-6 text-center p-3">
       <div class="area shadow-sm p-4">
         <a href="?option=3">
-          <img class="imagenArea items-center" src="../img/topicos/admon-red-icono.PNG" alt="">
+          <img class="areaImage items-center" src="../img/topicos/programacion-web-icono.PNG" alt="">
         </a>
-        <h3 class="tituloArea text-center font-bold text-xl">Redes y Telecomunicaciones</h3>
-        <p class="textoArea">Permite la interconexión entre distintos equipos de cómputo de forma física o virtual.</p>
+        <h3 class="areaTitle text-center font-bold text-xl">Programación web</h3>
+        <p class="areaText">Crea y desarrolla aplicaciones y sitios web para brindar soluciones a las empresas.</p>
       </div>
     </div>
 
     <div class="col-lg-4 col-sm-6 text-center p-3">
       <div class="area shadow-sm p-4">
         <a href="?option=3">
-          <img class="imagenArea items-center" src="../img/topicos/inteligencia-artificial-icono.PNG" alt="">
+          <img class="areaImage items-center" src="../img/topicos/admon-red-icono.PNG" alt="">
         </a>
-        <h3 class="tituloArea text-center font-bold text-xl">Inteligencia Artificial</h3>
-        <p class="textoArea">Aprende los fundamentos sobre sistemas informáticos de última generación.</p>
+        <h3 class="areaTitle text-center font-bold text-xl">Redes y Telecomunicaciones</h3>
+        <p class="areaText">Permite la interconexión entre distintos equipos de cómputo de forma física o virtual.</p>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-6 text-center p-3">
+      <div class="area shadow-sm p-4">
+        <a href="?option=3">
+          <img class="areaImage items-center" src="../img/topicos/inteligencia-artificial-icono.PNG" alt="">
+        </a>
+        <h3 class="areaTitle text-center font-bold text-xl">Inteligencia Artificial</h3>
+        <p class="areaText">Aprende los fundamentos sobre sistemas informáticos de última generación.</p>
       </div>
     </div>
 
@@ -157,49 +158,49 @@
 </section>
 <!-- Fin Áreas de aprendizaje-->
 
+
 <!-- Instalaciones -->
-<section class="seccionInstalaciones bg-dark p-2 px-4">
+<section class="darkSection bg-dark p-2 px-4">
   <div class="row mb-4 px-2 g-0">
 
     <div class="col-lg-6 col-12 py-4">
       <div class="d-flex justify-content-center align-items-center w-100 h-100">
         <div class="row g-0">
           <div class="col-12">
-            <h2 class="tituloSeccionOscura text-center font-bold m-3">Instalaciones</h2>
-            <div class="separadorSeccionOscura"></div>
+            <h2 class="titleDarkSection text-center font-bold m-3">Instalaciones</h2>
+            <div class="darkSectionSeparator"></div>
           </div>
-          <div class="col-12" style="text-align: center;">
+          <div class="col-12 px-4" style="text-align: center;">
             <p class="text-light" style="text-align: justify;">
-              El departamento de sistemas y computación cuenta con diversos
-              laboratorios de equipo de cómputo en dónde los estudiantes podrán
-              desempañarse en desarrollar diversos programas, aplicaciones y
-              proyectos a lo largo de sus clases prácticas en la carrera de
-              ingeniería en sistemas computacionales. <br /><br />
-              Los laboratorios de cómputo están orientados a servir como
-              herramientas de aprendizaje y apoyo didáctico tanto para los
-              docentes como los alumnos.
+              El departamento de sistemas y computación cuenta con diversos laboratorios de equipo de cómputo en dónde los 
+              estudiantes podrán desempañarse en desarrollar diversos programas, aplicaciones y proyectos a lo largo de sus 
+              clases prácticas en la carrera de ingeniería en sistemas computacionales. <br /><br />
+              
+              Los laboratorios de cómputo están orientados a servir como herramientas de aprendizaje y apoyo didáctico tanto 
+              para los docentes como los alumnos.
             </p>
-            <a class="btn btn-warning font-bold" href="?option=4" role="button">Ver más</a>
+            <a class="btn btn-warning font-bold" href="?option=4" role="button">¡Explorar!</a>
           </div>
         </div>
       </div>
     </div>
 
-    <div class="col-lg-6 col-sm-12 p-2 shadow-sm">
+    <div class="col-lg-6 col-12 p-4 shadow-sm">
       <div class="d-flex justify-content-center align-items-center w-100 h-100">
-        <img src="../img/IMG_2102.webp" class="img-fluid rounded p-1" alt="">
+        <img class="img-fluid rounded" src="../img/IMG_2102.webp" alt="">
       </div>
     </div>
+
   </div>
 </section>
 <!--Fin Instalaciones-->
 
-<!-- Comunidad de maestros  -->
+<!-- Comunidad docente  -->
 <section class="p-2 bg-light">
   <div class="row px-2 g-0">
 
     <div class="col-lg-6 col-sm-12">
-      <div class="contenedor-galeria bg-light">
+      <div class="galleryContainer bg-light">
         <div class="row g-0">
           <div class="col-4 px-2">
             <img class="img-fluid w-100 shadow-1-strong rounded m-2" src="../img/Docentes/abelardoRodriguezLeon.webp"
@@ -225,22 +226,20 @@
       </div>
     </div>
 
-    <div class="seccionComunidad col-lg-6 col-12 px-4">
+    <div class="lightSection col-lg-6 col-12 px-4">
       <div class="d-flex justify-content-center align-items-center w-100 h-100">
         <div class="row g-0">
           <div class="col-12">
-            <h2 class="tituloSeccion text-center font-bold m-3">Comunidad docente</h2>
-            <div class="separadorSeccion"></div>
+            <h2 class="sectionTitle text-center font-bold m-3">Comunidad docente</h2>
+            <div class="sectionSeparator"></div>
           </div>
           <div class="col-12" style="text-align: center;">
             <p style="text-align: justify;"> Nuestros profesores son impulsores de cambio, son el elemento esencial de
-              nuestra institución,
-              contamos con profesores y profesoras de diversas disciplinas, quienes demuestran diariamente su compromiso
-              más allá de las
-              aulas. Son profesionales que tienen la facultad de transformar vidas, de impactar a las futuras
-              generaciones de estudiantes.
+              nuestra institución, contamos con profesores y profesoras de diversas disciplinas, quienes demuestran diariamente 
+              su compromiso más allá de las aulas. Son profesionales que tienen la facultad de transformar vidas, de impactar a 
+              las futuras generaciones de estudiantes.
             </p>
-            <a class="btn btn-warning font-bold" href="?op=2" role="button">Explorar maestros</a>
+            <a class="btn btn-warning font-bold" href="?option=2" role="button">¡Conocer docentes!</a>
           </div>
 
         </div>
@@ -248,53 +247,54 @@
     </div>
   </div>
 </section>
-<!-- Fin Comunidad de maestros -->
+<!-- Fin Comunidad docente -->
 
-<!-- Areas de aprendizaje -->
-<section class="seccionArea p-2">
-  <h2 class="tituloSeccion text-center font-bold m-3">Vinculación</h2>
-  <div class="separadorSeccion"></div>
+<!-- Enlaces -->
+<section class="lightSection bg-light p-2">
+  <h2 class="sectionTitle text-center font-bold m-3">Enlaces</h2>
+  <div class="sectionSeparator"></div>
 
   <div class="row g-0">
-    <div class="col-lg-12 col-sm-12">
-      <div class="contenedor-galeria bg-light">
-        <div class="row g-0">
 
-          <div class="col-lg-4 col-sm-6 text-center p-3 shadow-sm area">
-            <a href="http://gestion.itver.edu.mx/servicio-social/">
-              <img class="img-fluid w-100 shadow-1-strong rounded mb-4 p-1" src="../img/IMG_1223.webp" alt="">
-            </a>
-            <h3 class="tituloArea text-center font-bold text-xl">Servicio social</h3>
-          </div>
-
-          <div class="col-lg-4 col-sm-6 text-center p-3 shadow-sm area">
-            <a href="http://www.veracruz.tecnm.mx/">
-              <img class="img-fluid w-100 shadow-1-strong rounded mb-4  p-1" src="../img/ITVER-9.webp" alt="">
-            </a>
-            <h3 class="tituloArea text-center font-bold text-xl">TECNM Campus Veracruz</h3>
-          </div>
-
-          <div class="col-lg-4 col-sm-6 text-center p-3 shadow-sm area">
-            <a href="http://www.veracruz.tecnm.mx/">
-              <img class="img-fluid w-100 shadow-1-strong rounded mb-4  p-1" src="../img/IMG_1245.webp" alt="">
-            </a>
-            <h3 class="tituloArea text-center font-bold text-xl">Comunidad de Ingeniería en Sistemas</h3>
-          </div>
-
-        </div>
+    <div class="col-lg-4 col-sm-6 text-center p-3">
+      <div class="area shadow-sm p-4 rounded-3">
+        <a href="http://gestion.itver.edu.mx/servicio-social/">
+          <img class="img-fluid w-100 shadow-1-strong rounded mb-4 p-1" src="../img/IMG_1223.webp" alt="">
+        </a>
+        <h3 class="areaTitle text-center font-bold text-xl">Servicio social</h3>
       </div>
     </div>
-  </div>
+
+    <div class="col-lg-4 col-sm-6 text-center p-3">
+      <div class="area shadow-sm p-4 rounded-3">
+        <a href="http://www.veracruz.tecnm.mx/">
+          <img class="img-fluid w-100 shadow-1-strong rounded mb-4  p-1" src="../img/ITVER-9.webp" alt="">
+        </a>
+        <h3 class="areaTitle text-center font-bold text-xl">TECNM Campus Veracruz</h3>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-6 text-center p-3">
+      <div class="area shadow-sm p-4 rounded-3">
+        <a href="?option=5">
+          <img class="img-fluid w-90 shadow-1-strong rounded mb-4  p-1" src="../img/IMG_1245.webp" alt="">
+        </a>
+        <h3 class="areaTitle text-center font-bold text-xl">Comunidades</h3>
+      </div>
+    </div>
+
+    
+
   </div>
 </section>
-<!-- Fin Áreas de aprendizaje-->
+<!-- Fin Enlaces ->
 
 <!-- Área de redes sociales -->
-<section class="seccionArea p-2 g-0">
+<section class="lightSection p-2 g-0">
   <div class="row g-0">
     <div class="col">
-      <h2 class="tituloSeccion text-center font-bold m-3">Redes sociales</h2>
-      <div class="separadorSeccion"></div>
+      <h2 class="sectionTitle text-center font-bold m-3">Redes sociales</h2>
+      <div class="sectionSeparator"></div>
     </div>
   </div>
 </section>
@@ -302,7 +302,7 @@
   <div class="row g-0">
     <div class="col-12 col-md-6">
 
-      <h4 class="tituloSeccion text-center font-bold">Tecnológico Nacional de México</h4>
+      <h4 class="sectionTitle text-center font-bold">Tecnológico Nacional de México</h4>
 
       <div class="d-flex flex-row justify-content-center">
         <a class="mx-2" href="https://www.tecnm.mx/" target="_blank" style="text-decoration: none;">
@@ -332,7 +332,7 @@
       </div>
     </div>
     <div class="col-12 col-md-6">
-      <h4 class="tituloSeccion text-center font-bold">Instituto Tecnológico de Veracruz</h4>
+      <h4 class="sectionTitle  text-center font-bold">Instituto Tecnológico de Veracruz</h4>
       <div class="d-flex flex-row justify-content-center">
         <a class="mx-2" href="https://www.veracruz.tecnm.mx/index.php" target="_blank" style="text-decoration: none;">
           <div class="d-flex flex-row justify-content-center">
@@ -363,7 +363,7 @@
     </div>
   </div>
   <div class="row g-0">
-    <h4 class="tituloSeccion text-center font-bold">Departamento de Sistemas Computacionales</h4>
+    <h4 class="sectionTitle  text-center font-bold">Departamento de Sistemas Computacionales</h4>
     <div class="d-flex flex-row justify-content-center">
       <a class="mx-2" href="https://www.facebook.com/profile.php?id=100065327956621" target="_blank"
         style="text-decoration: none;">
@@ -375,4 +375,4 @@
     </div>
   </div>
 </section>
-<!-- Fin área de redes sociales -->
+<!-- Fin Área de redes sociales -->
