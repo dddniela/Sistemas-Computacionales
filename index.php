5f7d119364394c7d293a838c9c76e7f6127985f5
Daniela Castro <daniela.castro@phs.mx>
2023-08-16 12:19:49 -0600

    update de index

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ $comunidad->setConnection($conn->getDB());
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="Ingeniería, Sistemas, Sistemas Computacionales, Ingeniería en Sistemas Computacionales">
-    <meta name="author" content="TECNM Veracruz">
+    <meta name="author" content="Tecnológico Nacional de México Veracruz">
     <link rel="icon" href="img/itver-logo.PNG"/>
 
     <link rel="stylesheet" href="assets/css/bootstrap.css">
